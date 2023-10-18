71899b1d065ebad5a7e4c93cd2dc162d30d48c1b
Karolina Ryzińska <76969276+kryzin@users.noreply.github.com>
2023-10-18 21:32:29 +0200

    0_01 database setup

---
+++ recipe_stash/recipes/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Recipe(models.Model):
+    TYPE_CHOICES = [
+        ('breakfast', 'breakfast'),
+        ('lunch', 'lunch'),
+        ('dinner', 'dinner'),
+        ('dessert', 'dessert'),
+        ('snack', 'snack'),
+        ('other', 'other'),
+    ]
+
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    preparation_time = models.DurationField()
+    overall_time = models.DurationField()
+    recipe_type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    ingredients = models.ManyToManyField(Ingredient, through='RecipeIngredient')
+    # rating = models.FloatField()
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.FloatField()
+    unit_of_measurement = models.CharField(max_length=20)  # add choices for units
+
+
+class Author(models.Model):  # later replace with AbstractUser or other auth alt
+    name = models.CharField(max_length=100)
+    username = models.CharField(max_length=50, unique=True)
+    bio = models.TextField()
+    public = models.BooleanField(default=True)
