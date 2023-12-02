b8e5a235bb8ba53e551f804e8f4ea6e2836051be
Karolina Ryzińska <76969276+kryzin@users.noreply.github.com>
2023-12-02 20:48:49 +0100

    base crud endpoints

--- recipe_stash/recipes/serializers.py
+++ recipe_stash/recipes/serializers.py
@@ -30,6 +30,20 @@ class IngredientSerializer(serializers.ModelSerializer):
         read_only_fields = ['id']
 
 
+class RecipeIngredientSerializer(serializers.ModelSerializer):
+    ingredient = IngredientSerializer()
+
+    class Meta:
+        model = RecipeIngredient
+        fields = ['recipe', 'ingredient', 'quantity', 'unit_of_measurement']
+
+    # def create(self, validated_data):
+    #     ingredient_data = validated_data.pop('ingredient')
+    #     ingredient = Ingredient.objects.create(**ingredient_data)
+    #     recipe_ingredient = RecipeIngredient.objects.create(ingredient=ingredient, **validated_data)
+    #     return recipe_ingredient
+
+
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
@@ -45,8 +59,22 @@ class CommentSerializer(serializers.ModelSerializer):
 
 class RecipeSerializer(serializers.ModelSerializer):
     comments = CommentSerializer(many=True, read_only=True)
+    recipe_ingredients = RecipeIngredientSerializer(many=True)
 
     class Meta:
         model = Recipe
         read_only_fields = ['id']
         fields = '__all__'
+
+    # def create(self, validated_data):
+    #     # this is so that you can create the ingredients while creating a recipe
+    #     # when writing a json request you would do:
+    #     # {"recipe":"Recipe1","ingredient":"{"name":"Flour"}, "quantity":"300", "unit_of_mesure":"grams"}
+    #     # the inner {} define a Ingredient Objects, the outer {} define a RecipeIngredient Object
+    #
+    #     recipe_ingredients_data = validated_data.pop('recipe_ingredients')
+    #     recipe = Recipe.objects.create(**validated_data)
+    #     for recipe_ingredient_data in recipe_ingredients_data:
+    #         # Recipe creates RecipeIngredient and RecipeIngredient creates Ingredient
+    #         RecipeIngredient.objects.create(recipe=recipe, **recipe_ingredient_data)
+    #     return recipe
--- recipe_stash/recipes/views.py
+++ recipe_stash/recipes/views.py
@@ -1,9 +1,8 @@
-# from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from rest_framework import status
 from rest_framework.response import Response
 from .models import Profile, Recipe
-from .serializers import RecipeSerializer, ProfileSerializer
+from .serializers import RecipeSerializer, ProfileSerializer, TagSerializer
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from django.core.exceptions import PermissionDenied
@@ -13,11 +12,9 @@ import copy
 
 # views still missing:
 """
-Create Recipe (with creating ingredients),
-All Recipes,
-CRUD Tag,
+CRUD for Ingredients etc.,
 Comments: CRUD + see them in Recipe,
-Register a new User -> Create a Profile,
+Register a new User -> Create a Profile (1-1 User-Profile),
 Recipes of given Author, or Profile with all it's recipes,
 Search for recipe by tags in url,
 Search for recipe by ingredients in url
@@ -62,6 +59,17 @@ class ProfileDetail(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
+class ProfileList(APIView):
+    """
+    + add auth, only show *my* profiles
+    All Profile Objects
+    """
+    def get(self, request, format=None):
+        profiles = Profile.objects.all()
+        serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
+
+
 class RecipeDetail(APIView):
     """
     + add auth that only author can edit
@@ -91,7 +99,66 @@ class RecipeDetail(APIView):
         print(serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def post(self, request, format=None):
+        serializer = RecipeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     def delete(self, request, pk, format=None):
         recipe = self.get_object(pk)
         recipe.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class RecipeList(APIView):
+    """
+    + everyone can see all recipes (of public accounts)
+    All Recipe Objects
+    """
+    def get(self, request, format=None):
+        recipes = Recipe.objects.all()
+        serializer = RecipeSerializer(recipes, many=True)
+        return Response(serializer.data)
+
+
+class TagDetail(APIView):
+    """
+    CRUD
+    Get/Update/Create/Delete a Tag object with given pk
+    """
+    def get_queryset(self):
+        return Tag.objects.all()
+
+    def get_object(self, pk):
+        try:
+            return Tag.objects.get(pk=pk)
+        except Tag.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        tag = self.get_object(pk)
+        serializer = TagSerializer(tag)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        tag = self.get_object(pk)
+        serializer = TagSerializer(tag, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        print(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def post(self, request, format=None):
+        serializer = TagSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        tag = self.get_object(pk)
+        tag.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
